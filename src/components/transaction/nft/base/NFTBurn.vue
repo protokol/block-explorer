c14682742c69a6c0a527bfc489d860ccc532458d
KovacZan <zan@protokol.com>
2021-02-17 22:08:20 +0100

    refactor: nft-base transactions

--- src/components/transaction/nft/base/NFTBurn.vue
+++ src/components/transaction/nft/base/NFTBurn.vue
@@ -1,21 +1,21 @@
 <template>
-  <Fragment>
-
-  </Fragment>
+  <section class="py-5 mb-5 page-section md:py-10">
+    <h3 class="px-5 sm:px-10">{{ $t(`TRANSACTION.NFT_BURN.BURN`) }}</h3>
+    <br />
+    <div class="px-5 sm:px-10">
+      <div class="list-row-border-b">
+        <div class="mr-4">{{ $t(`TRANSACTION.NFT_BURN.NFT_ID`) }}</div>
+        <LinkTransaction :id="transaction.asset.nftBurn.nftId" :truncate-id="false" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { ITransaction } from "@/interfaces";
-import VueJsonPretty from "vue-json-pretty";
-import Fragment from "@/components/utils/Fragment.vue";
 
-@Component({
-  components: {
-    VueJsonPretty,
-    Fragment,
-  },
-})
+@Component
 export default class NFTBurn extends Vue {
   @Prop({ required: true })
   public transaction: ITransaction;
--- src/components/transaction/nft/base/NFTTransfer.vue
+++ src/components/transaction/nft/base/NFTTransfer.vue
@@ -1,21 +1,32 @@
 <template>
-  <Fragment>
-
-  </Fragment>
+  <section class="py-5 mb-5 page-section md:py-10">
+    <h3 class="px-5 sm:px-10">{{ $t(`TRANSACTION.NFT_TRANSFER.TRANSFER`) }}</h3>
+    <br />
+    <div class="px-5 sm:px-10">
+      <div class="list-row-border-b">
+        <div class="mr-4">{{ $t(`TRANSACTION.NFT_TRANSFER.RECIPIENT_ID`) }}</div>
+        <LinkWallet :address="transaction.asset.nftTransfer.recipientId" :trunc="false" tooltip-placement="left" />
+      </div>
+      <div class="list-row-border-b">
+        <div class="mr-4">{{ $t(`TRANSACTION.NFT_TRANSFER.NFT_IDS`) }}</div>
+        <div>
+          <LinkTransaction
+            v-for="value in transaction.asset.nftTransfer.nftIds"
+            :id="value"
+            :key="value"
+            :truncate-id="false"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { ITransaction } from "@/interfaces";
-import VueJsonPretty from "vue-json-pretty";
-import Fragment from "@/components/utils/Fragment.vue";
 
-@Component({
-  components: {
-    VueJsonPretty,
-    Fragment,
-  },
-})
+@Component
 export default class NFTTransfer extends Vue {
   @Prop({ required: true })
   public transaction: ITransaction;
