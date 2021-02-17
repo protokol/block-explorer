012a644efc03e4b0c0b5a08a099ad72a667ecc6c
KovacZan <zan@protokol.com>
2021-02-17 23:18:30 +0100

    refactor: fix NFTDetail component

--- src/components/transaction/nft/NFTDetails.vue
+++ src/components/transaction/nft/NFTDetails.vue
@@ -2,7 +2,17 @@
   <Fragment>
     <template v-if="transaction.typeGroup === 9000">
       <NFTRegisterCollection v-if="transaction.type === 0" :transaction="transaction" />
-      <NFTCreate v-if="transaction.type === 1" :transaction="transaction" />
+      <NFTCreate v-else-if="transaction.type === 1" :transaction="transaction" />
+      <NFTTransfer v-else-if="transaction.type === 2" :transaction="transaction" />
+      <NFTBurn v-else-if="transaction.type === 3" :transaction="transaction" />
+    </template>
+
+    <template v-if="transaction.typeGroup === 9001">
+      <NFTAuction v-if="transaction.type === 0" :transaction="transaction" />
+      <NFTAuctionCancel v-else-if="transaction.type === 1" :transaction="transaction" />
+      <NFTBid v-else-if="transaction.type === 2" :transaction="transaction" />
+      <NFTBidCancel v-else-if="transaction.type === 3" :transaction="transaction" />
+      <NFTAcceptTrade v-else-if="transaction.type === 4" :transaction="transaction" />
     </template>
   </Fragment>
 </template>
@@ -15,12 +25,28 @@ import VueJsonPretty from "vue-json-pretty";
 import { ApiService, NFTService } from "@/services";
 import NFTRegisterCollection from "@/components/transaction/nft/base/NFTRegisterCollection.vue";
 import NFTCreate from "@/components/transaction/nft/base/NFTCreate.vue";
+import NFTTransfer from "@/components/transaction/nft/base/NFTTransfer.vue";
+import Fragment from "@/components/utils/Fragment.vue";
+import NFTBurn from "@/components/transaction/nft/base/NFTBurn.vue";
+import NFTAuction from "@/components/transaction/nft/exchange/NFTAuction.vue";
+import NFTAuctionCancel from "@/components/transaction/nft/exchange/NFTAuctionCancel.vue";
+import NFTBid from "@/components/transaction/nft/exchange/NFTBid.vue";
+import NFTBidCancel from "@/components/transaction/nft/exchange/NFTBidCancel.vue";
+import NFTAcceptTrade from "@/components/transaction/nft/exchange/NFTAcceptTrade.vue";
 
 @Component({
   components: {
+    NFTAcceptTrade,
+    NFTBidCancel,
+    NFTBid,
+    NFTAuctionCancel,
+    NFTAuction,
+    NFTBurn,
+    NFTTransfer,
     NFTCreate,
     NFTRegisterCollection,
     VueJsonPretty,
+    Fragment,
   },
 })
 export default class NFTDetails extends Vue {
@@ -31,24 +57,6 @@ export default class NFTDetails extends Vue {
   private NFTExchange = TypeGroupTransaction.NFT_EXCHANGE;
 
   private collectionName = "";
-
-  beforeCreate() {
-    console.log("before create");
-  }
-  created() {
-    console.log("created");
-  }
-
-  beforeMount() {
-    console.log("beforeMount");
-  }
-
-  public async mounted() {
-    console.log("mounted");
-    if (this.transaction.typeGroup === 9000 && this.transaction.type === 1) {
-      this.getCollection(this.transaction.asset.nftToken.collectionId);
-    }
-  }
 
   private async getCollection(id: string) {
     return (await NFTService.getCollection(id)).name;
