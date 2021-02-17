b667d04c41468c8f37db6db7d4301673fddbf903
KovacZan <zan@protokol.com>
2021-02-17 21:07:24 +0100

    feat: setup exchange components

---
+++ src/components/transaction/nft/exchange/NFTBid.vue
@@ -0,0 +1,21 @@
+<template>
+  <Fragment>
+
+  </Fragment>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { ITransaction } from "@/interfaces";
+import Fragment from "@/components/utils/Fragment.vue";
+
+@Component({
+  components: {
+    Fragment,
+  },
+})
+export default class NFTBid extends Vue {
+  @Prop({ required: true })
+  public transaction: ITransaction;
+}
+</script>
