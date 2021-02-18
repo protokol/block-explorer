3f1e08b764f5f003627188b2d4b4bb7cc69d42fb
KovacZan <zan@protokol.com>
2021-02-18 23:27:18 +0100

    feat: setup search components for nft-exchange

---
+++ src/components/search/nft/exchange/NFTAcceptTrade.vue
@@ -0,0 +1,10 @@
+<template>
+  <section></section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class NFTAcceptTrade extends Vue {}
+</script>
