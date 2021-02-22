3515db9d7641066123e30c2ba5d1e3ad480e51e8
KovacZan <zan@protokol.com>
2021-02-22 14:40:53 +0100

    feat: implement NFTAuctionCancel component

--- src/components/submit/nft/exchange/NFTAuctionCancelSubmit.vue
+++ src/components/submit/nft/exchange/NFTAuctionCancelSubmit.vue
@@ -7,40 +7,69 @@
       <div class="w-full lg:w-3/4">
         <InputText
           :label="$t(`SUBMIT_TRANSACTIONS.NFT_AUCTION_CANCEL.AUCTION_ID`)"
-          name="nft-auctionCancel-auctionId"
+          name="id"
           class="mr-8 my-3"
           @input="onInputChange"
         />
       </div>
     </div>
-    <div class="flex flex-wrap justify-between mb-4">
-      <div class="w-full">
-        <InputText
-          :label="$t(`SUBMIT_TRANSACTIONS.VENDORFIELD`)"
-          name="vendorField"
-          class="mr-8 my-3"
-          @input="onInputChange"
-        />
-      </div>
-    </div>
-    <div class="flex flex-wrap justify-between mb-4">
-      <div class="w-full">
-        <InputText
-          :label="$t(`SUBMIT_TRANSACTIONS.PASSPHRASE`)"
-          :value="properties['passphrase']"
-          name="passphrase"
-          class="mr-8 my-3"
-          @input="onInputChange"
-        />
-      </div>
-    </div>
+
+    <PassphraseVFSubmit
+      :response-success="responseSuccess"
+      :response-error="responseError"
+      @submit="submitTransaction"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { ApiService, WalletService } from "@/services";
+import { Identities } from "@arkecosystem/crypto";
+import { Builders } from "@protokol/nft-exchange-crypto";
+import PassphraseVFSubmit from "@/components/submit/Passphrase-VFSubmit.vue";
+
 @Component({
-  components: {},
+  components: {
+    PassphraseVFSubmit,
+  },
 })
-export default class NFTAuctionCancelSubmit extends Vue {}
+export default class NFTAuctionCancelSubmit extends Vue {
+  private id = "";
+
+  private responseSuccess: string = null;
+  private responseError: string = null;
+
+  private onInputChange(event: any): void {
+    const { name, value } = event.target;
+
+    if (name === "id") {
+      this.id = value.toString();
+    }
+  }
+
+  private async submitTransaction({ passphrase, vendorField }): Promise<void> {
+    try {
+      const nonce = await WalletService.fetchNextNonce(Identities.Address.fromPassphrase(passphrase));
+      const transaction = new Builders.NFTAuctionCancelBuilder()
+        .NFTAuctionCancelAsset({
+          auctionId: this.id,
+        })
+        .nonce(nonce)
+        .vendorField(vendorField)
+        .sign(passphrase);
+
+      const broadcastResponse = await ApiService.post("transactions", { transactions: [transaction.getStruct()] });
+
+      this.response = broadcastResponse.data;
+      if (broadcastResponse.data.accept.length > 0) {
+        this.responseSuccess = broadcastResponse.data.accept[0];
+      } else {
+        this.responseError = "Something went wrong";
+      }
+    } catch {
+      this.responseError = "Something went wrong";
+    }
+  }
+}
 </script>
